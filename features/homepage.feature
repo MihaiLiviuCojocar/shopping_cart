3d8700136782501223082f210b000a7a4fff4995
MihaiLiviuCojocar <cmihailiviu@yahoo.com>
2015-10-02 13:06:59 +0100

    Created Homepage feature.

---
+++ features/homepage.feature
@@ -0,0 +1,5 @@
+Feature: Homepage
+
+  Scenario: Visiting this awesome shopping site
+    Given I visit the home page
+    Then I should see a welcome message
---
+++ features/step_definitions/homepage_steps.rb
@@ -0,0 +1,7 @@
+Given(/^I visit the home page$/) do
+  visit '/'
+end
+
+Then(/^I should see a welcome message$/) do
+  pending # express the regexp above with the code you wish you had
+end
