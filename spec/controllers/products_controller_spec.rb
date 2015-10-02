1648abda59dc1e0ebca7eec53b7c79374eca3fc6
MihaiLiviuCojocar <cmihailiviu@yahoo.com>
2015-10-02 13:42:01 +0100

    First test passed... everything linked properly.

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe ProductsController, type: :controller do
+  describe 'GET #show' do
+    before { get :index }
 
+    it { should render_template('index') }
+  end
 end
